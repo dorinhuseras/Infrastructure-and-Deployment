535e1a836aa68f7f29628e5b06833b7ff8341004
dorinhuseras <147391203+dorinhuseras@users.noreply.github.com>
2026-02-19 20:51:34 +0200

    Updated LAb 4

--- 4.Configuring and Managing Virtual Networks/TfLabCheckpoint/variables.tf
+++ 4.Configuring and Managing Virtual Networks/TfLabCheckpoint/variables.tf
@@ -1,4 +1,4 @@
-variable "name_prefix" { 
+variable "name_prefix" {
   type        = string
   description = "Prefix for all resource names"
 }
@@ -6,10 +6,10 @@ variable "name_prefix" {
 variable "location" { type = string }
 
 variable "vnet_octet" {
-  type = number
+  type        = number
   description = "The middle octet of the VNet (10.X.0.0/16)"
 }
 
-variable "vm_size"        { type = string }
+variable "vm_size" { type = string }
 variable "admin_username" { type = string }
 variable "admin_password" { type = string }
